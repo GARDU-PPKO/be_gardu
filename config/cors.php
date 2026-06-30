1d0809f4009873a46eeedf0c55e7d281dc071924
ilhammaulana <xilhammaulanax739@gmail.com>
2026-06-30 16:28:21 +0700

    tambah origin fe-gardu.vercel.app di CORS

--- config/cors.php
+++ config/cors.php
@@ -3,7 +3,7 @@
 return [
     'paths' => ['api/*'],
     'allowed_methods' => ['*'],
-    'allowed_origins' => [env('APP_URL', 'http://localhost'), 'http://localhost:5173', 'http://localhost:5174', 'http://127.0.0.1:5173'],
+    'allowed_origins' => [env('APP_URL', 'http://localhost'), 'http://localhost:5173', 'http://localhost:5174', 'http://127.0.0.1:5173', 'https://fe-gardu.vercel.app'],
     'allowed_origins_patterns' => [],
     'allowed_headers' => ['*'],
     'exposed_headers' => [],
